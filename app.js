d93710196e1780e5feffed1800ba34175578ee8d
deefourple <davedehghani@gmail.com>
2017-03-14 18:32:42 -0400

    GSAP animations added to each song button

--- app.js
+++ app.js
@@ -26,6 +26,26 @@ $(document).ready(function(){
     src: ['songs/08.mp3'],
   });
 
+  var startingPostion = function() {
+    TweenLite.to($('#btn-01'), 5, {x: 0,  y: 0})
+    TweenLite.to($('#btn-02'), 4, {x: 0,  y: 0})
+    TweenLite.to($('#btn-03'), 3, {x: 0,  y: 0})
+    TweenLite.to($('#btn-04'), 9, {x: 0, y: 0})
+    TweenLite.to($('#btn-05'), 8, {x: 0,  y: 0})
+    TweenLite.to($('#btn-06'), 6, {x: 0,  y: 0})
+    TweenLite.to($('#btn-07'), 7, {x: 0, y: 0})
+    TweenLite.to($('#btn-08'), 10, {x: 0, y: 0})
+  }
+
+  TweenLite.to($('#btn-01'), 5, {x: 250, y: 300, ease:Elastic.easeOut,  onComplete: startingPostion})
+  TweenLite.to($('#btn-02'), 4, {x: 1000, y: 400, ease:Elastic.easeOut, onComplete: startingPostion})
+  TweenLite.to($('#btn-03'), 3, {x: 50, y: 60, ease:Elastic.easeOut,  onComplete: startingPostion})
+  TweenLite.to($('#btn-04'), 9, {x: 25, y: 330, ease:Elastic.easeOut, onComplete: startingPostion})
+  TweenLite.to($('#btn-05'), 8, {x: 505, y: 0, ease:Elastic.easeOut,  onComplete: startingPostion})
+  TweenLite.to($('#btn-06'), 6, {x: 420, y: 420, ease:Elastic.easeOut,  onComplete: startingPostion})
+  TweenLite.to($('#btn-07'), 7, {x: 10, y: 200, ease:Elastic.easeOut, onComplete: startingPostion})
+  TweenLite.to($('#btn-08'), 3, {x: -400, y: 100, ease:Elastic.easeOut, onComplete: startingPostion})
+
   //songs overplay eachother
   $('#btn-01').click(function(){
     one.stop();
@@ -61,13 +81,28 @@ $(document).ready(function(){
   });
 
   $('#play').click(function(){
-    sound.play();
+    //switch statement may be the best option
+    five.play()
   });
   $('#pause').click(function(){
-    sound.pause();
+    one.pause();
+    two.pause();
+    three.pause();
+    four.pause();
+    five.pause();
+    six.pause();
+    seven.pause();
+    eight.pause();
   });
   $('#stop').click(function(){
-    //switch statement may be the way to go
+    one.stop();
+    two.stop();
+    three.stop();
+    four.stop();
+    five.stop();
+    six.stop();
+    seven.stop();
+    eight.stop();
   });
 });
 
